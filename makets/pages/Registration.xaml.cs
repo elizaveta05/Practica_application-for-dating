9986dac4eee13079435ef15b5fe44cd2c4fb685b
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-23 15:15:20 +0700

    Создание регистрации

--- makets/pages/Registration.xaml.cs
+++ makets/pages/Registration.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Server.Model.Model_users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +22,15 @@ namespace makets.pages
     /// </summary>
     public partial class Registration : Window
     {
+        private readonly HttpClient _httpClient;
+
         public Registration()
         {
             InitializeComponent();
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7036/api/")
+            };
         }
 
         private void Label_MouseUp(object sender, MouseButtonEventArgs e)
@@ -31,22 +40,45 @@ namespace makets.pages
             this.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            //добавить проверку полей
-            if (PassB1.Password == PassB2.Password)
+            if (string.IsNullOrEmpty(tb_login.Text) || string.IsNullOrEmpty(PassB1.Password) || string.IsNullOrEmpty(PassB2.Password))
             {
-                MessageBox.Show("Пароли совпадают. Регистрация успешна!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Все поля должны быть заполнены", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (PassB1.Password != PassB2.Password)
+            {
+                MessageBox.Show("Пароли не совпадают", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var user = new UserDataRegister
+            {
+                Login = tb_login.Text,
+                Password = PassB1.Password
+            };
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("Autho/register", content);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Регистрация успешна!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Переход на страницу приветствия
                 WelcomePagexaml welcomWindow = new WelcomePagexaml();
                 welcomWindow.Show();
                 this.Close();
-
             }
             else
             {
-                MessageBox.Show("Пароли не совпадают. Пожалуйста, попробуйте снова.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                string error = await response.Content.ReadAsStringAsync();
+                MessageBox.Show($"Ошибка: {error}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
         }
+
     }
 }
