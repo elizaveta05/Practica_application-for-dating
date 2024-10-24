c60cbf84a9c5a85e3d5715079ec15c90f5f9a15e
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-24 17:27:01 +0700

    Реализация авторизации

--- makets/MainWindow.xaml.cs
+++ makets/MainWindow.xaml.cs
@@ -1,52 +1,66 @@
 ﻿using makets.pages;
-using System.Text;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace makets
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HttpClient _httpClient;
+
         public MainWindow()
         {
             InitializeComponent();
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7036/api/")
+            };
         }
+        /*
+            В разметке нужно поставить на пароль PasswordBox 
+         */
 
-        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            PasswordReset passwordReset = new PasswordReset();
-            passwordReset.Show();
-            this.Close();
-        }
+            // Получаем логин и пароль из текстовых полей
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordTextBox.Text.Trim();
 
-        private void Label_MouseUp_1(object sender, MouseButtonEventArgs e)
-        {
-            Registration registrationWindow = new Registration();
-            registrationWindow.Show();
-            this.Close();
-        }
+            // Формируем запрос
+            var request = new
+            {
+                Login = login,
+                Password = password
+            };
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            /*
-            //логика проверки логина, пароля 
-            WelcomePagexaml welcomWindow = new WelcomePagexaml(0);
-            welcomWindow.Show();
-            this.Close();
-            */
-            UserInfo window = new UserInfo(2);
-            window.Show();
-            this.Close();
+            // Отправляем POST запрос на сервер для авторизации
+            var response = await _httpClient.PostAsJsonAsync("Authorization/authorization", request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = await response.Content.ReadFromJsonAsync<dynamic>();
+                MessageBox.Show("Авторизация успешна!");
+                int userId = responseData.userId;
+                if (userId.Equals(null))
+                {
+                    UserProfile window = new UserProfile(userId);
+                    window.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось авторизоваться. Попробуйте еще раз.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                MessageBox.Show($"Ошибка авторизации: {errorMessage}");
+            }
         }
     }
 }
